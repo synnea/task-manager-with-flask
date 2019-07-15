2bd967e950b57ffc35492bdc76b21740a9df76fb
Carina <carrie.poell@gmail.com>
2019-07-15 13:27:13 +0200

    set up Flask

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello world!"
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP'), port=(os.getenv('PORT')))
